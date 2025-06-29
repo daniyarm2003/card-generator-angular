a8dbbacca2b7d73642ea4418c03e86140fc88516
Daniyar Mukhamedgaliyev <daniyar.m2003@gmail.com>
2025-06-28 22:17:31 -0400

    Refactor card and types page templates to use DefaultLayoutComponent and enhance loading indication

--- src/app/pages/card-page/card-page.component.ts
+++ src/app/pages/card-page/card-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { DefaultLayoutComponent } from "../../layouts/default-layout/default-layout.component";
 
 @Component({
   selector: 'app-card-page',
-  imports: [],
+  imports: [DefaultLayoutComponent],
   templateUrl: './card-page.component.html',
   styleUrl: './card-page.component.scss'
 })
 export class CardPageComponent {
-
+  public cardsLoading = false;
 }
