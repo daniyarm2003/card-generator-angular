ec023229a26d07d4cf3febf60cb537717b70eb25
Daniyar Mukhamedgaliyev <daniyar.m2003@gmail.com>
2026-06-10 17:21:44 -0400

    Update version number

---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  imports: [CommonModule],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.scss'
+})
+export class SearchBarComponent {
+  
+}
